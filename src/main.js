5972f991691c60e6f718d48b6619b85584a17c7e
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-20 08:19:47 +0530

    Modifying root element to accomodate bulma and navbar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue/dist/vue.esm-bundler'
 import { createPinia } from 'pinia'
 
 import './style.css'
+import 'bulma/css/bulma.css'
 import router from './router';
 const vueApplication = createApp({})
 vueApplication.use(router).use(createPinia()).mount('#app')
