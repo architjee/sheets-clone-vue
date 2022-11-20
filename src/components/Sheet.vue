eeddb51025f779d5afa2d49aff28cd6b0694e9e1
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-20 10:10:48 +0530

    Sheet

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -12,20 +12,29 @@ export default {
       rows_size: 8,
       cols_size: 8
     }
-  }
+  },
+  methods: {
+    getRowLabel(row_idx) {
+      return String.fromCharCode(row_idx+('A'.charCodeAt(0)));
+    }
+  },
 }
 </script>
 
 <template>
   <table class="table is-bordered is-narrow is-hoverable ">
-    <tr  v-for="row in Array(rows_size).keys()">
-      
-        <td v-for="col in Array(cols_size).keys()" initialcontent="" class="padding0"><Cell></Cell></td>
-      
-    
+    <thead>
+    <tr>
+      <th v-for="row in Array(rows_size).keys()">{{ getRowLabel(row)}}</th>
+    </tr>
+  </thead>
+    <tr v-for="row in Array(rows_size).keys()">
+      <td v-for="col in Array(cols_size).keys()" initialcontent="" class="padding0">
+        <Cell></Cell>
+      </td>
     </tr>
 
-    
+
   </table>
 </template>
 <style>
