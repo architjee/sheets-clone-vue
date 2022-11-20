b1be1adb972e629c646b0f0fae731851c315693e
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-20 09:13:36 +0530

    Navbar wiped and contained into its own component

---
+++ src/core_components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div id="navbarBasicExample">
+            <div class="navbar-start">
+                <router-link class="navbar-link" to="/">
+  <img src="../assets/vue.svg" width="22" alt="Bulma">
+                </router-link>
+                <router-link class="navbar-link" to="/">Home</router-link>
+                <router-link to="/sheet" class="navbar-link">Sheet (+)</router-link>
+            </div>
+        </div>
+    </nav>
+
+</template>
+    
+<script>
+
+export default {
+    name: 'navbar',
+
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+    
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import { createPinia } from 'pinia'
 import './style.css'
 import 'bulma/css/bulma.css'
 import router from './router';
-const vueApplication = createApp({})
+
+import Navbar from './core_components/Navbar.vue'
+const vueApplication = createApp({
+    components: {
+        'my-navbar': Navbar
+    }
+})
 vueApplication.use(router).use(createPinia()).mount('#app')
